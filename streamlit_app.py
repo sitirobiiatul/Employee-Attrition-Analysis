f5a4a66e1f05ceaac70abfbca0632c165e4a7b48
sitirobiiatul <73388631+sitirobiiatul@users.noreply.github.com>
2025-05-06 15:21:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ Original file is located at
 !pip install streamlit
 
 import streamlit as st
-import pandas as pd
 import numpy as np
 import joblib
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
