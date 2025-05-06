5e1f922c8b0e267f60ef1d302b7cd142ca9ad5ab
sitirobiiatul <73388631+sitirobiiatul@users.noreply.github.com>
2025-05-06 15:21:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1diIwWPAXCE3HIgL3TH4H8LPLasU4-FhZ
 """
 
-!pip install streamlit
 
 import streamlit as st
 import numpy as np
